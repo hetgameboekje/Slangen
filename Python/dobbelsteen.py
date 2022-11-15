1042d08a7d0499a58d94e698d2b8e65da1a7472e
HetGameBoekje <514527@vistacollege.nl>
2022-11-15 13:31:16 +0100

    yes

--- Python/dobbelsteen.py
+++ Python/dobbelsteen.py
@@ -1,11 +1,15 @@
+# Importeren van de random module
 import random
+# Functie om een willekeurig getal te genereren
+def getal():
+#vraag gebruiker om aantal dobbelstenen
+    aantal = int(input("Hoeveel dobbelstenen wil je gooien? "))
+    for i in range(aantal):
+        print(random.randint(1,6))
 
-def rollen(min, max):
-    while True:
-        print(f"Jouw dobbelsteen is {random.randint(min, max)}")
-        answer = input("Wil je nog een keer rollen? (y/n) ")
-        if answer == "n":
-            break
+#geef gebruiker het aantal ogen van de dobbelstenen
+getal()
+#vraag gebruiker of hij nog een keer wil gooien
 
-rollen(1, 6)
-   
+#als gebruiker ja zegt, doe het nog een keer
+#als gebruiker nee zegt, stop
