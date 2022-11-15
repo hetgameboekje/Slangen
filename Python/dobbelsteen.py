4082e7f88c355649805cd120cf50c16b2c55dd7b
HetGameBoekje <514527@vistacollege.nl>
2022-11-15 13:20:25 +0100

    Dobbelsteen werkt

--- Python/dobbelsteen.py
+++ Python/dobbelsteen.py
@@ -3,9 +3,9 @@ import random
 def rollen(min, max):
     while True:
         print(f"Jouw dobbelsteen is {random.randint(min, max)}")
-        # answer = input("Wil je nog een keer rollen? (y/n) ")
-        # if answer == "n":
-        #     break
+        answer = input("Wil je nog een keer rollen? (y/n) ")
+        if answer == "n":
+            break
 
 rollen(1, 6)
    
