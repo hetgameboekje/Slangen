4a16f7ef7d080dbf2159d43918251d1e3acfa0ab
HetGameBoekje <514527@vistacollege.nl>
2022-11-17 11:24:59 +0100

    Het werkt?

--- Python/Dobbelsteen/dobbelsteen2.py
+++ Python/Dobbelsteen/dobbelsteen2.py
@@ -1,7 +1,8 @@
 import random
 
+max = int(input("Hoeveel ogen heeft de dobbelsteen maximaal "))
 totaalDobbel = []
-
 aantal = int(input("Hoeveel dobbelstenen wil je gooien? "))
 for i in range(aantal):
-        print(random.randint(1,6))
+    print("Dobbelsteen",(i + 1),"gooide" ,random.randint(1,max) )
+# totaalDobbel = sum(totaalDobbel)
---
+++ Python/looptien.py
@@ -0,0 +1,4 @@
+# opdracht getallen 5
+
+for i in range (1,11):
+    print(i*10)
