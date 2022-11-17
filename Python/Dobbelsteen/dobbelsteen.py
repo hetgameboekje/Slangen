06aacdd0feb98803e49c0d694b9fac3dce52037c
HetGameBoekje <514527@vistacollege.nl>
2022-11-17 10:57:49 +0100

    rekenmachine goed + dobbelsteen update

--- Python/Dobbelsteen/dobbelsteen.py
+++ Python/Dobbelsteen/dobbelsteen.py
@@ -10,3 +10,16 @@ def getal():
 
 getal()
 #functie om gebruiker te vragen of ze het opnieuw willen doen
+
+
+import random
+
+def rollen(min, max):
+    while True:
+        print(f"Jouw dobbelsteen is {random.randint(min, max)}")
+        answer = input("Wil je nog een keer rollen? (y/n) ")
+        if answer == "n":
+            break
+
+rollen(1, 6)
+   
