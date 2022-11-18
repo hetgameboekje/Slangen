219b18016582a1379ded2b8cf73d7949aa390d64
HetGameBoekje <514527@vistacollege.nl>
2022-11-18 12:37:10 +0100

    Create test2.py

---
+++ Roulette/test2.py
@@ -0,0 +1,8 @@
+totaalchips = 10
+winchips = 35
+totaalchips = (totaalchips + winchips)
+print("je hebt", totaalchips, "chips totaal!")
+totaalchips = (totaalchips + winchips)
+print("je hebt", totaalchips, "chips totaal!")
+totaalchips = (totaalchips + winchips)
+print("je hebt", totaalchips, "chips totaal!")
