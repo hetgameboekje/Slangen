e856d844e4eecaca4fc05a0ce7e4566876bf9e9b
HetGameBoekje <514527@vistacollege.nl>
2022-11-18 12:49:13 +0100

    Update test2.py

--- Roulette/test2.py
+++ Roulette/test2.py
@@ -6,10 +6,12 @@ totaalchips = (totaalchips + winchips)
 print("je hebt", totaalchips, "chips totaal!")
 totaalchips = (totaalchips + winchips)
 print("je hebt", totaalchips, "chips totaal!")
-roll = 1
+roll = 2
 
 if roll == 1:
         totaalchips = (totaalchips + winchips)
         print("je hebt", totaalchips, "chips totaal!")
 else:
         print('You lost')
+        totaalchips = (totaalchips - totaalchips)
+        print("je hebt", totaalchips, "chips totaal!")
