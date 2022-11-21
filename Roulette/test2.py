c338c9c007db1e2e56cadd734255df9f6482ba04
HetGameBoekje <514527@vistacollege.nl>
2022-11-21 10:43:27 +0100

    Update test2.py

--- Roulette/test2.py
+++ Roulette/test2.py
@@ -2,20 +2,24 @@ import random
 
 totaalchips = 10
 winchips = 35
+inzet = 1
+
 
 print("je hebt", totaalchips, "chips totaal!")
-print ("Je kan 35 chips winnen, Totaal is dit", totaalchips+ winchips,"chips")
+print("Je kan 35 chips winnen, Totaal is dit", totaalchips + winchips, "chips")
 print("-=-=-=-=-=-=-=-=-=-=-")
 
-rollguess = random.randint(1,5)
+for i in range(totaalchips):
+        rollguess = random.randint(1, 2)
 
-roll = int(input('Place your bet, 1-5: '))
-print('-=-=-=-=-=-=-=-=-=-=-')
-if roll == rollguess:
-        totaalchips = (totaalchips + winchips)
-        print("Gefeliciteerd! je succesvol geraden, je hebt", totaalchips, "chips totaal!")
-        
-else:
-        print("Spel rolde:", rollguess)
-        totaalchips = 0
-        print("je hebt verloren, je hebt", totaalchips, "chips totaal! ")
+        roll = int(input('Place your bet, 1-36: '))
+        print('-=-=-=-=-=-=-=-=-=-=-')
+        if roll == rollguess:
+                totaalchips = (totaalchips + winchips)
+                print("Gefeliciteerd! je succesvol geraden, je hebt", totaalchips, "chips totaal!")
+
+        else:
+                print("Spel rolde:", rollguess)
+                totaalchips = (totaalchips - inzet)
+                print("je hebt verloren, je hebt", totaalchips, "chips totaal! ")
+                
