f616c18ebd82653dfd26d6a81e9dcfbcaecda9cc
HetGameBoekje <514527@vistacollege.nl>
2022-11-28 11:35:09 +0100

    Added rijbewijs2

--- Rijbewijs nr 2/Rijbewijs2.py
+++ Rijbewijs nr 2/Rijbewijs2.py
@@ -1,3 +1,15 @@
-print("waneer bent u geboren?")
-datum = input("Zet de datum dag/maand/jaar ")
-if datum =
+import datetime
+now = datetime.datetime.now()
+print ("Current date and time : ")
+print (now.strftime("%Y-%m-%d %H:%M:%S"))
+print("1 april 2018 = 20180401")
+s = input("whats your birthday")
+s_datetime = datetime.datetime.strptime(s, '%Y%m%d')
+print (s_datetime)
+while True:
+    if s_datetime < now:
+        print("you are older than 18")
+        break
+    else:
+        print("you are younger than 18")
+        break
