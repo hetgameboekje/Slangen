e450cd42aab2f52961ec6144059f57f42c2f7211
HetGameBoekje <514527@vistacollege.nl>
2022-12-02 09:10:16 +0100

    vo

--- Dobbelsteen/dobbelsteen.py
+++ Dobbelsteen/dobbelsteen.py
@@ -4,6 +4,6 @@ max = int(input("Hoeveel ogen heeft de dobbelsteen maximaal "))
 totaalDobbel = []
 aantal = int(input("Hoeveel dobbelstenen wil je gooien? "))
 for i in range(aantal):
-    print("Dobbelsteen",(i + 1),"gooide" ,random.randint(1,max) )
+    print("Dobbelsteen",(i + 2),"gooide" ,random.randint(1,max) )
     totaalDobbel.append(random.randint(1,max))
 print("je hebt totaal", sum(totaalDobbel), "ogen gegooid")
